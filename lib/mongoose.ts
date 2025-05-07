6d741a6e443c5da9c6e44e957d9ee4d4968e031c
Relebohile Sekutlu <relebohilesekutlu26@gmail.com>
2025-05-08 00:37:48 +0200

    Fix build issues and incorrect imports223

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,33 +1,16 @@
-import mongoose from 'mongoose'
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
 
-const MONGODB_URI = process.env.MONGODB_URI!
-
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local')
-}
-
-let cached = global.mongoose
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
-}
-
-export const connectToDatabase=()=> {
-  // If connection already exists, return it
-  if (cached.conn) return cached.conn
-
-  // If no promise is cached, initiate the connection and cache the promise
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
-      dbName: 'iwb', // Specify the database name
-      bufferCommands: false, // Disable buffering of commands when disconnected
+      dbName: 'iwb',
+      bufferCommands: false,
     }).catch(error => {
-      console.error("Error connecting to MongoDB:", error) // Log connection error
-      throw new Error("MongoDB connection failed") // Propagate the error
-    })
+      console.error("Error connecting to MongoDB:", error);
+      throw new Error("MongoDB connection failed");
+    });
   }
 
-  // Wait for the connection promise to resolve
-  cached.conn = await cached.promise
-  return cached.conn
-}
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
